b1104a9918ac3bf0da2ada4ff53e4804fda9ff36
vanessuniq <vanessuniq@gmail.com>
2020-07-04 18:28:14 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,39 +5,39 @@ class Post < ActiveRecord::Base
   belongs_to :author
 
   #put new code here
-  #def self.filter(author_id, date)
-   # if author_id.present? && date.present?
-    #  if date == "Today"
-     #   where({author_id: author_id, created_at: Time.zone.today.beginning_of_day})
-      #else
-       # where("author_id=? and created_at < ?",author_id, Time.zone.today.beginning_of_day)
-      #end
-    #elsif author_id.present?
-     # where(author: author_id)
+  def self.filter(author_id, date)
+    if author_id.present? && date.present?
+      if date == "Today"
+        where("author_id=? and created_at >= ?", author_id, Time.zone.today.beginning_of_day)
+      else
+        where("author_id=? and created_at < ?", author_id, Time.zone.today.beginning_of_day)
+      end
+    elsif author_id.present?
+      where(author: author_id)
       
-    #elsif date.present?
-     # if date == "Today"
-      #  where("created_at >=?", Time.zone.today.beginning_of_day)
-      #else
-       # where("created_at <?", Time.zone.today.beginning_of_day)
-      #end
+    elsif date.present?
+      if date == "Today"
+        where("created_at >=?", Time.zone.today.beginning_of_day)
+      else
+        where("created_at <?", Time.zone.today.beginning_of_day)
+      end
       
-    #else
-      #self.all
-    #end
+    else
+      self.all
+    end
+  end
+
+  #def self.by_author(author_id)
+   # where(author_id: author_id)
   #end
 
-  def self.by_author(author_id)
-    where(author_id: author_id)
-  end
-
-  def self.from_today
-    where("created_at >=?", Time.zone.today.beginning_of_day)
-  end
+  #def self.from_today
+  #  where("created_at >=?", Time.zone.today.beginning_of_day)
+  #end
    
-  def self.old_news
-    where("created_at <?", Time.zone.today.beginning_of_day)
-  end
+  #def self.old_news
+  #  where("created_at <?", Time.zone.today.beginning_of_day)
+  #end
 
   private
 
